179e9b68011cefae74886b1939e420ba88afbb75
Lucas Narcizo Neto <lucasnarcizo99@hotmail.com>
2022-05-27 17:44:56 -0300

    adicionando rotas novas pra busca edicao e delecao

--- app.js
+++ app.js
@@ -27,4 +27,41 @@ app.get("/mangas", (req, resp)=> {
 
 });
 
+app.get("/mangas/:id", (req, resp)=> {
+
+    const {id} = req.params;
+
+    const manga = mangas.find(manga => manga.id === id);
+
+    return resp.json(manga);
+
+});
+
+app.put("/atualiza-manga/:id", (req, resp)=> {
+
+    const { id } = req.params;
+    const { nome, capitulo } = req.body;
+
+    const mangaIndex = mangas.findIndex(manga => manga.id === id);
+
+    mangas[mangaIndex] = {
+        id: mangas[mangaIndex],
+        nome,
+        capitulo
+    };
+
+    return resp.json({message: "Manga atualizado com sucesso"});
+});
+
+app.delete("/deletar-manga/:id", (req, resp)=> {
+
+    const {id} = req.params;
+
+    const mangaIndex = mangas.findIndex(manga => manga.id === id);
+
+    mangas.splice(mangaIndex, 1);
+
+    return resp.json({message: "Manga excluido com sucesso!"})
+});
+
 app.listen(5000, () => console.log("servidor esta aqui port:5000"));
